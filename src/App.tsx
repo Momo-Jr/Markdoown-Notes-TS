36fec35a3402da2ed697b49c664239161c5911ae
Mohamed Mohsen <mohamed.nashaat1@vodafone.com>
2023-05-12 15:56:58 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<h1> This should work </h1>}></Route>
+    </Routes>
+  );
+}
