2b36ca3381d7114eae506d46a078ff6aefd814a1
Mohamed-Mohsen <mohamed.nashaat1@vodafone.com>
2023-05-12 21:48:52 +0300

    More Changes

--- src/pages/NoteList.tsx
+++ src/pages/NoteList.tsx
@@ -12,7 +12,6 @@ import {
 import { Link } from 'react-router-dom';
 import ReactSelect from 'react-select';
 import { Tag } from '../App';
-import styles from './NoteList.module.css';
 
 type SimplifiedNote = {
   tags: Tag[];
@@ -136,7 +135,7 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
     <Card
       as={Link}
       to={`/${id}`}
-      className={`h-100 text-reset text-decoration-none ${styles.card}`}
+      className={`h-100 text-reset text-decoration-none`}
     >
       <Card.Body>
         <Stack
